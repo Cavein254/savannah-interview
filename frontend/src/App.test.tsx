7ee0139877c86ff65844f6aefd12f89f5fc5646b
johnmuthua <caveomuthama@gmail.com>
2024-11-29 01:33:30 +0300

    chore: configure vitest

---
+++ frontend/src/App.test.tsx
@@ -0,0 +1,7 @@
+import { describe, it, expect } from "vitest";
+
+describe("2 plus 2 test", () => {
+  it("should be equal to 4", () => {
+    expect(2 + 2).toEqual(4);
+  });
+});
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslintPlugin from "@nabla/vite-plugin-eslint";
@@ -5,4 +8,9 @@ import eslintPlugin from "@nabla/vite-plugin-eslint";
 
 export default defineConfig({
   plugins: [react(), eslintPlugin()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/tests/setup.ts",
+  },
 });
