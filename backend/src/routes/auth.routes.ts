65a3ae5b3b7f8e2bef52aad497a54848ab332d43
johnmuthua <caveomuthama@gmail.com>
2024-11-29 22:58:35 +0300

    feat: add database schema

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import passport from "./passport";
+import passport from "../auth/passport";
 import prisma from "../lib/prisma";
 
 const authRouter = express.Router();
