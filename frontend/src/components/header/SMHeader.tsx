87875df2551895440076afcc25c321639cb4f173
johnmuthua <caveomuthama@gmail.com>
2024-12-01 19:36:20 +0300

    feat: add header for small devices

--- frontend/src/components/header/SMHeader.tsx
+++ frontend/src/components/header/SMHeader.tsx
@@ -2,6 +2,9 @@ import { User } from "@/types"
 import { HiOutlineLockClosed } from "react-icons/hi2"
 import { Link } from "react-router-dom"
 import HeaderDropdown from "../dropdown/HeaderDropdown"
+import { FaWheatAwn } from "react-icons/fa6"
+import { RxHamburgerMenu } from "react-icons/rx"
+import { ModeToggle } from "../theme/mode-toggle"
 
 const VITE_SERVER_LOGIN = import.meta.env.VITE_SERVER_LOGIN as string
 
@@ -11,9 +14,20 @@ interface SMHeaderProps {
 const SMHeader = ({ user }: SMHeaderProps) => {
   return (
     <div className="md:hidden flex justify-between">
-      <div className="">logo</div>
-      <div className="">logo77</div>
       <div className="">
+        <button>
+          <RxHamburgerMenu size={24} />
+        </button>
+      </div>
+      <div className="">
+        <Link to="/">
+          <button>
+            <FaWheatAwn size={24} />
+          </button>
+        </Link>
+      </div>
+      <div className="flex items-center gap-2">
+        <ModeToggle />
         {user ? (
           <HeaderDropdown image={user.image} />
         ) : (
